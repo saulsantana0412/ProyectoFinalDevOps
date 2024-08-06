c7669446a83d40841bb0a85f88e61f1d03421e26
saulsantana0412 <saulsantan0412@gmail.com>
2024-08-06 11:49:39 -0400

    Agregando recursos de pagina web Hola Mundo

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".auto-type",{
+    strings: ["Hola Mundo!", "Hello World!", "Bonjour le monde!", "你好世界！", "Olá Mundo!"],
+    typedSpeed: 1000,
+    backSpeed: 100,
+    showCursor:  false,
+    loop: true,
+});
